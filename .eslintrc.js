53badb694ddf5134f21f6d46c9813095634ff01e
Umair Ahmed <i211182@nu.edu.pk>
2024-04-25 23:45:24 +0500

    code rev

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@eslint/js/recommended', // Assuming this is a valid plugin configuration
+  ],
+  parserOptions: {
+    ecmaVersion: 12, // or 2020
+    sourceType: 'module',
+  },
+  rules: {
+    // Define your rules here
+  },
+  globals: {
+    globalsVar1: 'readonly', // Example of setting a global variable
+  },
+  plugins: [
+    '@eslint/js', // Assuming this is the name of a valid plugin
+  ],
+};
